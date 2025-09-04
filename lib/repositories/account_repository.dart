f03497f49dbc98d126c2ad7c8f52a0de3c5f2a1b
reinhart-wilson <118143693+reinhart-wilson@users.noreply.github.com>
2025-09-04 16:29:50 +0700

    Update account_repository.dart
    Repos now extends ChangeNotifier so all changes will be notify listeners (viewmodels)

--- lib/repositories/account_repository.dart
+++ lib/repositories/account_repository.dart
@@ -3,31 +3,40 @@ import 'dart:async';
 import 'package:finance_tracker/models/account.dart';
 import 'package:finance_tracker/services/local_data_service.dart';
 import 'package:finance_tracker/models/mappers/account_mapper.dart';
+import 'package:flutter/foundation.dart';
 
-class AccountRepository {
-  AccountRepository(this._localDataService);
+class AccountRepository with ChangeNotifier {
+  AccountRepository({required localDataService})
+      : _localDataService = localDataService;
 
   final LocalDataService _localDataService;
 
   Future<List<Account>> getAllAccounts() async {
     final accountMaps = await _localDataService.fetchAccounts();
     return accountMaps.map((accountMap) => accountMap.fromMap()).toList();
-  } 
+  }
 
-  Future<int> addAccount(Account account) async{
+  Future<int> addAccount(Account account) async {
     try {
       int id = await _localDataService.insertAccount(account.toMap());
+      notifyListeners();
       return id;
-    } catch (e){
+    } catch (e) {
       rethrow;
     }
   }
 
   Future<void> deleteAccount(int accountId) async {
-    try{
+    try {
       await _localDataService.deleteAccount(accountId);
-    } catch (e){
+      notifyListeners();
+    } catch (e) {
       rethrow;
     }
+  }
+
+  Future<List<Account>> getChildAccounts(int parentId) async {
+    final childAccounts = await _localDataService.fetchAccounts(parentId: parentId);
+    return childAccounts.map((account) => account.fromMap()).toList();
   }
 }
