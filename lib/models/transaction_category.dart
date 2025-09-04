ed9f8636b099e9eefa7268ea9327eb230209d412
reinhart-wilson <118143693+reinhart-wilson@users.noreply.github.com>
2025-09-04 16:28:11 +0700

    Update transaction_category.dart
    Changed erroneus attribute data type

--- lib/models/transaction_category.dart
+++ lib/models/transaction_category.dart
@@ -1,14 +1,12 @@
 class TransactionCategory {
-  final String id;
+  final int? id;
   final String name;
-  final String type;
   final int? defaultAccountId;
   final String? color;
 
   TransactionCategory({
-    required this.id,
+    this.id,
     required this.name,
-    required this.type,
     this.defaultAccountId,
     this.color,
   });
