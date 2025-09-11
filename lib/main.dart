ed2e02630168bad9f56c8e7d04899ff45713ba8c
reinhart-wilson <118143693+reinhart-wilson@users.noreply.github.com>
2025-09-11 17:03:18 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:finance_tracker/views/home_view.dart';
 import 'package:finance_tracker/views/transaction_list_view.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
 import 'package:finance_tracker/configs/dependencies.dart';
 import 'package:finance_tracker/views/account_list_view.dart';
@@ -9,43 +10,11 @@ import 'package:finance_tracker/views/account_list_view.dart';
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
 
-  runApp(MultiProvider(
-      providers: buildProviders(),
-      child: const MaterialApp(
-        home: HomeView(),
-      )));
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.lightBlue),
-        useMaterial3: true,
-      ),
-      home: const HomeView(),
-    );
-  }
+  runApp(MultiProvider(providers: buildProviders(), child: const MyHomePage()));
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  // final String title;
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
@@ -69,56 +38,15 @@ class _MyHomePageState extends State<MyHomePage> {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return PopScope(
-      canPop: false, // biar kita kontrol sendiri
-      onPopInvokedWithResult: (didPop, _) async {
-        if (didPop) return;
-
-        final isFirstRouteInCurrentTab =
-            !await _navigatorKeys[_selectedIndex].currentState!.maybePop();
-
-        if (isFirstRouteInCurrentTab) {
-          if (_selectedIndex != 0) {
-            setState(() {
-              _selectedIndex = 0;
-            });
-            return; // jangan keluar app
-          }
-          // biarkan keluar app kalau sudah di tab 0
-          SystemNavigator.pop();
-        }
-      },
-      child: Scaffold(
-        body: IndexedStack(
-          index: _selectedIndex,
-          children: [
-            Navigator(
-              key: _navigatorKeys[0],
-              onGenerateRoute: (settings) =>
-                  MaterialPageRoute(builder: (_) => const Text('Home')),
-            ),
-            Navigator(
-              key: _navigatorKeys[1],
-              onGenerateRoute: (settings) => MaterialPageRoute(
-                  builder: (_) => const TransactionListView()),
-            ),
-            Navigator(
-              key: _navigatorKeys[2],
-              onGenerateRoute: (settings) =>
-                  MaterialPageRoute(builder: (_) => const AccountListView()),
-            ),
-          ],
-        ),
-        bottomNavigationBar: BottomNavigationBar(
-          currentIndex: _selectedIndex,
-          onTap: _onTap,
-          items: const [
-            BottomNavigationBarItem(icon: Icon(Icons.home), label: "Beranda"),
-            BottomNavigationBarItem(icon: Icon(Icons.list), label: "Transaksi"),
-            BottomNavigationBarItem(icon: Icon(Icons.wallet), label: "Akun"),
-          ],
-        ),
-      ),
+    final TextTheme textTheme = Theme.of(context).textTheme;
+    return MaterialApp(
+      theme: ThemeData(
+          textTheme: GoogleFonts.interTextTheme().copyWith(
+              displaySmall: GoogleFonts.manrope(
+                  fontWeight: FontWeight.bold,
+                  color: Theme.of(context).colorScheme.onPrimary,
+                  textStyle: textTheme.displaySmall))),
+      home: const HomeView(),
     );
   }
 }
