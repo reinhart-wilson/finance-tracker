2b699a37d5d37f2dd970b7ea61add44f29b38332
reinhart-wilson <118143693+reinhart-wilson@users.noreply.github.com>
2025-09-16 13:43:59 +0700

    Update transaction_list_item.dart
    Minor cosmetic tweak

--- lib/views/widgets/transaction/transaction_list_item.dart
+++ lib/views/widgets/transaction/transaction_list_item.dart
@@ -64,7 +64,7 @@ class TransactionItemTile extends StatelessWidget {
       margin: const EdgeInsets.symmetric(vertical: 2),
       color: !isSettled
           ? Colors.orange.withOpacity(0.05)
-          : const Color.fromARGB(255, 240, 228, 255).withOpacity(0.4),
+          : Theme.of(context).colorScheme.onPrimary,
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(12),
       ),
